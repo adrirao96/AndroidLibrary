d40d2565856697c4d6f0417cc89d01f6e740a3b1
adrianrao <adrirao.96@gmail.com>
2023-07-28 23:25:03 -0300

    Fix

--- app/src/main/java/com/arao/androidlibrary/MainActivity.kt
+++ app/src/main/java/com/arao/androidlibrary/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.arao.androidlibrary.ui.theme.AndroidLibraryTheme
-import com.arao.ui.ArTextField
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArTextField()
+                    //ArTextField()
                 }
             }
         }
